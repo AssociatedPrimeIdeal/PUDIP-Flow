184dab0879d4366dee76d0d544e2f047b3752dbe
Yuyang Ren <149291174+AssociatedPrimeIdeal@users.noreply.github.com>
2025-04-29 03:27:18 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+1. download the testdata at https://drive.google.com/file/d/1hor9kU9INUdgSCXTS02tTMCwETN7Q6cl/view?usp=sharing
+2. run test.ipynb
---
+++ unet_pudipflow.py
@@ -0,0 +1,111 @@
+import torch.nn.functional as F
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class DoubleConv(nn.Module):
+    """(convolution => [BN] => ReLU) * 2"""
+
+    def __init__(self, in_channels, out_channels, mid_channels=None):
+        super().__init__()
+        if not mid_channels:
+            mid_channels = out_channels
+        self.double_conv = nn.Sequential(
+            nn.Conv3d(in_channels, mid_channels, kernel_size=3, padding=1),
+            nn.InstanceNorm3d(mid_channels),
+            nn.PReLU(),
+            nn.Conv3d(mid_channels, out_channels, kernel_size=3, padding=1),
+            nn.InstanceNorm3d(out_channels),
+            nn.PReLU()
+        )
+
+    def forward(self, x):
+        return self.double_conv(x)
+
+
+class Down(nn.Module):
+    """Downscaling with maxpool then double conv"""
+
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.maxpool_conv = nn.Sequential(
+            nn.MaxPool3d(2),
+            DoubleConv(in_channels, out_channels)
+        )
+
+    def forward(self, x):
+        return self.maxpool_conv(x)
+
+
+class Up(nn.Module):
+    """Upscaling then double conv"""
+
+    def __init__(self, in_channels, out_channels, bilinear=True):
+        super().__init__()
+
+        # if bilinear, use the normal convolutions to reduce the number of channels
+        if bilinear:
+            self.up = nn.Upsample(scale_factor=2, mode='nearest')
+            self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
+        else:
+            self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
+            self.conv = DoubleConv(in_channels, out_channels)
+
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        # input is CHW
+        diffZ = x2.size()[-3] - x1.size()[-3]
+        diffY = x2.size()[-2] - x1.size()[-2]
+        diffX = x2.size()[-1] - x1.size()[-1]
+
+        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
+                        diffY // 2, diffY - diffY // 2,
+                        diffZ // 2, diffZ - diffZ // 2,])
+        # if you have padding issues, see
+        # https://github.com/HaiyongJiang/U-Net-Pytorch-Unstructured-Buggy/commit/0e854509c2cea854e247a9c615f175f76fbb2e3a
+        # https://github.com/xiaopeng-liao/Pytorch-UNet/commit/8ebac70e633bac59fc22bb5195e513d5832fb3bd
+        x = torch.cat([x2, x1], dim=1)
+        return self.conv(x)
+
+
+class OutConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(OutConv, self).__init__()
+        self.conv = nn.Conv3d(in_channels, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        return self.conv(x)
+
+
+class UNet3D_PUDIPFlow(nn.Module):
+    def __init__(self, n_channels, n_classes, ft, bilinear=True):
+        super(UNet3D_PUDIPFlow, self).__init__()
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.bilinear = bilinear
+
+        self.inc = DoubleConv(n_channels, ft)
+        self.down1 = Down(ft, ft * 2 ** 1)
+        self.down2 = Down(ft * 2 ** 1, ft * 2 ** 2)
+        self.down3 = Down(ft * 2 ** 2, ft * 2 ** 3)
+        factor = 2 if bilinear else 1
+        self.down4 = Down(ft * 2 ** 3, ft * 2 ** 4 // factor)
+        self.up1 = Up(ft * 2 ** 4, ft * 2 ** 3 // factor, bilinear)
+        self.up2 = Up(ft * 2 ** 3, ft * 2 ** 2 // factor, bilinear)
+        self.up3 = Up(ft * 2 ** 2, ft * 2 ** 1 // factor, bilinear)
+        self.up4 = Up(ft * 2 ** 1, ft, bilinear)
+        self.outc = OutConv(ft, n_classes)
+
+    def forward(self, x):
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        logits = self.outc(x)
+        return logits
